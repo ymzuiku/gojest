d7f6089f87110e81fd0d2e75b42fc3218990fbdb
pillar <ymblender@gmail.com>
2022-07-20 07:47:56 +0800

    fix: pwd path error

--- pwd/pwd.go
+++ pwd/pwd.go
@@ -3,13 +3,14 @@ package pwd
 import (
 	"log"
 	"os"
+	"path/filepath"
 )
 
 var pwd = ""
 
 func Load() string {
 	if pwd == "" {
-		file, err := os.Getwd()
+		file, err := filepath.Abs(filepath.Dir(os.Args[0]))
 		if err != nil {
 			log.Fatalln(err)
 		}
